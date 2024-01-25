3baf12657b2395d9391f6fff12c078052264540c
Walid Oumechtak <woumecht@e1r13p12.1337.ma>
2024-01-25 18:59:13 +0100

    fetch data from the api prepare to add them to the DOM

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // wheather api key: 90743435001043998ee145417242301
 // https://api.weatherapi.com/v1/current.json?key=11111111111111111&q=london
 
-async function getData()
+async function getData(city = "marrakech")
 {
-    var data = await fetch("https://api.weatherapi.com/v1/current.json?key=90743435001043998ee145417242301&q=london").then(data => data.json());
+    var data = await fetch(`https://api.weatherapi.com/v1/current.json?key=90743435001043998ee145417242301&q=${city}`).then(data => data.json());
     var obj = {
         current: data.current,
         location: data.location
@@ -11,8 +11,37 @@ async function getData()
     return obj;
 }
 
-let dataPromiss = getData();
+const searchSubmit = document.querySelector("[name=city]");
+let city = "";
+let dataPromiss;
+dataPromiss = getData();
+searchSubmit.addEventListener('keydown', (e) => {
+
+    if (e.key === 'Enter' || e.keyCode === 13)
+    {
+        city = searchSubmit.value;
+        console.log(city);
+        dataPromiss = getData(city);
+        dataPromiss.then((data) => {
+            buildComponent(data);
+        });
+    }
+})
+
 
 dataPromiss.then((data) => {
-    console.log(data);
+    buildComponent(data);     
 });
+
+
+function buildComponent(data)
+{
+    const status = data.current.condition.text;
+    const date = data.current.last_updated;
+    const nameOfTheCity = data.location.name;
+    const temp_c = data.current.temp_c; 
+    const temp_f = data.current.temp_f;
+    const feelsLike_c = data.current.feelslike_c; 
+    const feelsLike_f = data.current.feelslike_f;
+    console.log(data);
+}
